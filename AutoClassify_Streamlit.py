981ac00b198c155139af79a7d1486ac9abd7fc32
andrew-asgc <78685736+andrew-asgc@users.noreply.github.com>
2021-02-07 16:40:22 +0400

    Update AutoClassify_Streamlit.py

--- AutoClassify_Streamlit.py
+++ AutoClassify_Streamlit.py
@@ -13,8 +13,6 @@ identify = pd.read_csv('identify.csv')
 
 st.write('''# IT Expenditure Auto Classification''')
 
-
-
 option = st.selectbox(
 'How would you like to enter the data',
 ('Individual Input', 'Batch Input'))
@@ -22,7 +20,6 @@ option = st.selectbox(
 if option=='Individual Input':
 	st.write('''### Enter Item Description''')
 	new_item = st.text_input('Type below:')
-	print ('\nMost likely Sub Class: ', model.predict(vectorizer.transform([new_item])))
 	identify['Probability (%)'] = model.predict_proba(vectorizer.transform([new_item]))[0]*100
 	df = identify.sort_values(by='Probability (%)', ascending=False)
 	df.columns = [['Line Type','UNSPSC Class Name','Probability (%)']]
@@ -30,7 +27,6 @@ if option=='Individual Input':
 	st.write('''### The most likely UNSPSC Class for the above item is '''+'**'+df.iloc[0,1]+'**'+' with '+'**'+str(df.iloc[0,-1])+'**'+'% confidence.')
 	st.write('''### The following are the top 5 most likely UNSPSC Classes''')
 	st.write(df.head().reset_index(drop=True))
-
 else:
 	st.write('''### Upload Excel File''')
 	file = st.file_uploader("Choose an excel file", type="xlsx")
