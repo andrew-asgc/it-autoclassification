c12c18d1f7b2b932c406731bfdc77f8996c4f541
andrew-asgc <78685736+andrew-asgc@users.noreply.github.com>
2021-02-08 12:32:41 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"andrew.iskandar@asgcgroup.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
