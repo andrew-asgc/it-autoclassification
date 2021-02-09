9a135ba78e9fe319b8404e72d33866399e35bc9c
andrew-asgc <78685736+andrew-asgc@users.noreply.github.com>
2021-02-09 09:02:34 +0400

    Update AutoClassifyStreamlit.py

--- AutoClassifyStreamlit.py
+++ AutoClassifyStreamlit.py
@@ -9,28 +9,30 @@ model = pickle.load(open('it_auto_classification.sav', 'rb'))
 identify = pd.read_csv('identify.csv')
 st.markdown('<style>.reportview-container{background: url(https://github.com/andrew-asgc/it-autoclassification/blob/main/Picture1.png?raw=true); background-size: cover;}</style>',unsafe_allow_html=True)
 
-st.write('''# IT Expenditure Auto Classification''')
+st.title('''IT Expenditure Auto Classification''')
 
-option = st.sidebar.selectbox(
-'How would you like to enter the data',
-('Individual Input', 'Batch Input'))
+option = st.sidebar.radio(
+"Input Method:",
+('Individual Input','Batch Input'))
 
 if option=='Individual Input':
 	st.write('''### Enter Item Description''')
 	new_item = st.text_input('Type below:')
 	if new_item=='':
 		st.write('')
-	else:
+	else:	
 		identify['Probability (%)'] = model.predict_proba(vectorizer.transform([new_item]))[0]*100
 		df = identify.sort_values(by='Probability (%)', ascending=False)
 		df.columns = [['Line Type','UNSPSC Class Name','Probability (%)']]
 
-		st.write('''### The most likely UNSPSC Class for the above item is '''+'**'+df.iloc[0,1]+'**'+' with '+'**'+str(df.iloc[0,-1])+'**'+'% confidence.')
-		st.write('''### The following are the top 5 most likely UNSPSC Classes''')
-		st.table(df[['UNSPSC Class Name','Probability (%)']].head().reset_index(drop=True))
+		st.write('''The most likely UNSPSC Class for the above item is '''+'**'+df.iloc[0,1]+'**'+' with '+'**'+str(df.iloc[0,-1])+'**'+'% confidence.')
+		st.empty()
+		with st.beta_expander('See other options'):
+			st.write('''### The following are the top 5 most likely UNSPSC Classes''')
+			st.table(df[['UNSPSC Class Name','Probability (%)']].head().reset_index(drop=True))
 else:
 	st.write('''
-		### To Auto-Classify a batch, please upload an Excel file with each Item Description listed in *1 column only*.
+		To Auto-Classify a batch, please upload an Excel file with each Item Description listed in **_1 column only_**.
 		''')
 	st.write('''### Upload Excel File''')
 	file = st.file_uploader("Choose an excel file", type="xlsx")
@@ -43,5 +45,6 @@ else:
 		read_file['UNSPSC Class Name'] = model.predict(vectorizer.transform(read_file.iloc[:,0].values.flatten()))
 		read_file['Probability (%)'] = model.predict_proba(vectorizer.transform(read_file.iloc[:,0].values.flatten())).max(1)*100		
 		
-		st.write('### The following are the most likely UNSPSC Class Names for the uploaded Item Descriptions:')
+		st.write('The following are the most likely UNSPSC Class Names for the uploaded Item Descriptions:')
 		st.table(read_file)
+
